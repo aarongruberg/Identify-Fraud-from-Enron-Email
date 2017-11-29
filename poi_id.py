0b391cf5182d817ea3b1f507002dfc3741a5f909
Aaron-Lewis-Gruberg <aarongruberg@gmail.com>
2017-11-29 14:48:57 -0800

    Created more scaled features and applied selectKBest feature selection.

--- poi_id.py
+++ poi_id.py
@@ -8,11 +8,15 @@ from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
 import matplotlib.pyplot as plt
 
-from outliers import find_max
+from helper import find_max, computePercent
+
+from sklearn.feature_selection import SelectKBest
+
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
+
 ### restricted stock is stock that is given from employer
 ### to employee and cannot be transferred.
 ### Stock deferrals delay delivery of shares to employee 
@@ -22,15 +26,24 @@ from outliers import find_max
 ### deferred income in money that is received upfront 
 ### but reported in installments i.e. annual fee 12,000
 ### is reported as 1,000 each month.
-features_list = ['poi','salary', 'from_this_person_to_poi', \
- 'from_poi_to_this_person', 'total_stock_value', \
- 'deferral_payments', 'restricted_stock_deferred', \
- 'deferred_income', 'total_payments', 'loan_advances', \
- 'bonus', 'expenses', 'exercised_stock_options', \
- 'other', 'long_term_incentive', 'restricted_stock',\
- 'director_fees', 'to_messages', 'from_messages',
- 'shared_receipt_with_poi', 'percent_exercised_stock', \
- 'percent_to_poi', 'percent_from_poi'] # You will need to use more features
+
+### Created new features and excluded any original features
+### that contained redundant information such as 'from_this_person_to_poi'
+### or 'from_messages'.
+
+### COMPLETE SCALED FEATURES LIST.  Remember that 'poi' is a label.
+# features_list = ['poi','salary', 'deferred_income', 'loan_advances', \
+#  'other', 'long_term_incentive', 'percent_exercised_stock', \
+#  'percent_restricted_stock', 'percent_restricted_stock_deferred', \
+#  'percent_to_poi', 'percent_from_poi', 'percent_shared_with_poi', \
+#  'percent_deferral_payments', 'percent_expenses', 'percent_director_fees', \
+#  'percent_bonus'] # You will need to use more features
+
+### SELECTED FEATURES LIST
+### Univariate feature selection with selectKBest
+features_list = ['poi', 'salary', 'deferred_income', \
+'loan_advances', 'other', 'long_term_incentive', \
+'percent_to_poi', 'percent_shared_with_poi', 'percent_bonus']
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
@@ -40,8 +53,10 @@ with open("final_project_dataset.pkl", "r") as data_file:
 
 # many outliers in this dataset are important and should be kept
 # find_max revealed 'TOTAL' to have the max value
-# for financial features.  Remove the 'TOTAL' key. 
+# for financial features.  Remove the 'TOTAL' key.
+# There was another non person key 'THE TRAVEL AGENCY IN THE PARK' 
 data_dict.pop('TOTAL', 0)
+data_dict.pop('THE TRAVEL AGENCY IN THE PARK', 0)
 
 # find the max/min value and person for a feature
 #print find_max(data_dict, 'bonus')
@@ -50,36 +65,57 @@ data_dict.pop('TOTAL', 0)
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
 
-# new feature 'percent_exercised_stock'
 for person in data_dict.keys():
+	# new feature 'percent_exercised_stock'
 	num = data_dict[person]['exercised_stock_options']
 	den = data_dict[person]['total_stock_value']
-
-	if den != 0 and num != 'NaN' and den != 'NaN':
-		data_dict[person]['percent_exercised_stock'] = float(num)/float(den)
-	else:
-		data_dict[person]['percent_exercised_stock'] = 'NaN'
-
-# new feature 'percent_to_poi'
-for person in data_dict.keys():
+	data_dict[person]['percent_exercised_stock'] = computePercent(num, den)
+	
+	# new feature 'percent_restricted_stock'
+	num = data_dict[person]['restricted_stock']
+	den = data_dict[person]['total_stock_value']
+	data_dict[person]['percent_restricted_stock'] = computePercent(num, den)
+	
+	# new feature 'percent_restricted_stock_deferred'
+	num = data_dict[person]['restricted_stock_deferred']
+	den = data_dict[person]['total_stock_value']
+	data_dict[person]['percent_restricted_stock_deferred'] = computePercent(num, den)
+	
+	# new feature 'percent_to_poi'
 	num = data_dict[person]['from_this_person_to_poi']
 	den = data_dict[person]['from_messages']
+	data_dict[person]['percent_to_poi'] = computePercent(num, den)
 
-	if den != 0 and num != 'NaN' and den != 'NaN':
-		data_dict[person]['percent_to_poi'] = float(num)/float(den)
-	else:
-		data_dict[person]['percent_to_poi'] = 'NaN' 
-
-# new feature 'percent_from_poi'
-for person in data_dict.keys():
+	# new feature 'percent_from_poi'
 	num = data_dict[person]['from_poi_to_this_person']
 	den = data_dict[person]['to_messages']
+	data_dict[person]['percent_from_poi'] = computePercent(num, den)
 
-	if den != 0 and num != 'NaN' and den != 'NaN':
-		data_dict[person]['percent_from_poi'] = float(num)/float(den)
-	else:
-		data_dict[person]['percent_from_poi'] = 'NaN'
+	# new feature 'percent_shared_with_poi'
+	num = data_dict[person]['shared_receipt_with_poi']
+	den = data_dict[person]['from_messages']
+	den += data_dict[person]['to_messages']
+	data_dict[person]['percent_shared_with_poi'] = computePercent(num, den)
 
+	# new feature 'percent_deferral_payments'
+	num = data_dict[person]['deferral_payments']
+	den = data_dict[person]['total_payments']
+	data_dict[person]['percent_deferral_payments'] = computePercent(num, den)
+
+	# new feature 'percent_expenses'
+	num = data_dict[person]['expenses']
+	den = data_dict[person]['total_payments']
+	data_dict[person]['percent_expenses'] = computePercent(num, den)
+
+	# new feature 'percent_director_fees'
+	num = data_dict[person]['director_fees']
+	den = data_dict[person]['total_payments']
+	data_dict[person]['percent_director_fees'] = computePercent(num, den)
+
+	# new feature 'percent_bonus'
+	num = data_dict[person]['bonus']
+	den = data_dict[person]['salary']
+	data_dict[person]['percent_bonus'] = computePercent(num, den)
 
 
 my_dataset = data_dict
@@ -89,34 +125,22 @@ data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
 
 
+### Feature selection using selectKBest
+kbest = SelectKBest(k=8)
+selected_features = kbest.fit_transform(features, labels)
+print "8 best features:"
+print "--------------------------------------------------------------- "
+
+for i in kbest.get_support(indices=True):
+	print features_list[i+1]
+	
+
+
 # Visualize data
 for point in data:
-	poi = point[0] 
-	salary = point[1]
-	to_poi = point[2]
-	from_poi = point[3]
-	total_stock = point[4]
-	deferral_payments = point[5]
-	restricted_stock_deferred = point[6]
-	deferred_income = point[7]
-	total_payments = point[8]
-	loan_advances = point[9]
-	bonus = point[10]
-	expenses = point[11]
-	exercised_stock_options = point[12]
-	other = point[13]
-	long_term_incentive = point[14]
-	restricted_stock = point[15]
-	director_fees = point[16]
-	to_messages = point[17]
-	from_messages = point[18]
-	shared_receipt_with_poi = point[19]
-	percent_exercised_stock = point[20]
-	percent_to_poi = point[21]
-	percent_from_poi = point[22]
-	plt.scatter(poi, percent_from_poi)
+	plt.scatter(point[0], point[2])
 
-plt.show()
+#plt.show()
 
 
 ### Task 4: Try a varity of classifiers
@@ -140,21 +164,18 @@ clf.fit(features, labels)
 pred = clf.predict(features)
 acc = accuracy_score(pred, labels)
 print "GaussianNB accuracy:", acc
-print "------------------------------------------------------------------------"
 
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
 pred = clf.predict(features)
 acc = accuracy_score(pred, labels)
 print "Decision Tree overfitting accuracy:", acc
-print "------------------------------------------------------------------------"
 
 clf = svm.SVC()
 clf.fit(features, labels)
 pred = clf.predict(features)
 acc = accuracy_score(pred, labels)
 print "SVC accuracy:", acc
-print "------------------------------------------------------------------------"
 
 clf = neighbors.KNeighborsClassifier()
 clf.fit(features, labels)
@@ -177,6 +198,32 @@ from sklearn.cross_validation import train_test_split
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
 
+print "After training and testing:" 
+print "------------------------------------------------------------------------"
+
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+acc = accuracy_score(pred, labels_test)
+print "GaussianNB accuracy:", acc
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+acc = accuracy_score(pred, labels_test)
+print "Decision Tree overfitting accuracy:", acc
+
+clf = svm.SVC()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+acc = accuracy_score(pred, labels_test)
+print "SVC accuracy:", acc
+
+clf = neighbors.KNeighborsClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+acc = accuracy_score(pred, labels_test)
+print "KNeighborsClassifier accuracy:", acc
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
